8209c3146936b26f01dc8fa16e45827707b2881b
trivernis <trivernis@protonmail.com>
2021-04-18 16:57:24 +0200

    Add more information to mc item command
    Signed-off-by: trivernis <trivernis@protonmail.com>

--- src/commands/minecraft/item.rs
+++ src/commands/minecraft/item.rs
@@ -1,8 +1,10 @@
 use serenity::client::Context;
-use serenity::framework::standard::{macros::command, Args, CommandError, CommandResult};
+use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::channel::Message;
 
 use crate::commands::common::handle_autodelete;
+use crate::messages::minecraft::item::create_item_message;
+use crate::providers::minecraft::get_item_full_information;
 use crate::utils::context_data::Store;
 
 #[command]
@@ -18,50 +20,10 @@ pub(crate) async fn item(ctx: &Context, msg: &Message, args: Args) -> CommandRes
 
     let item_name = args.message().to_lowercase();
     log::debug!("Searching for item '{}'", item_name);
-    let items_by_name = store.minecraft_data_api.items.items_by_name()?;
-    let item = items_by_name
-        .get(&item_name)
-        .ok_or(CommandError::from(format!(
-            "The item `{}` could not be found",
-            item_name
-        )))?;
-    let enchantments_by_category = store
-        .minecraft_data_api
-        .enchantments
-        .enchantments_by_category()?;
-    log::trace!("Item is {:?}", item);
+    let information = get_item_full_information(&item_name, &store.minecraft_data_api)?;
+    log::trace!("Item full information is {:?}", information);
+    create_item_message(ctx, msg.channel_id, information).await?;
 
-    msg.channel_id
-        .send_message(ctx, |m| {
-            m.embed(|mut e| {
-                e = e
-                    .title(&*item.display_name)
-                    .thumbnail(format!(
-                        "https://minecraftitemids.com/item/128/{}.png",
-                        item.name
-                    ))
-                    .field("Name", &*item.name, false)
-                    .field("Stack Size", item.stack_size, false);
-                if let Some(durability) = item.durability {
-                    e = e.field("Durability", durability, true);
-                }
-                if let Some(variations) = &item.variations {
-                    e = e.field("Variations", format!("{:?}", variations), false);
-                }
-                if let Some(enchant_categories) = &item.enchant_categories {
-                    let item_enchantments = enchant_categories
-                        .into_iter()
-                        .filter_map(|c| enchantments_by_category.get(c))
-                        .flatten()
-                        .map(|e| e.display_name.clone())
-                        .collect::<Vec<String>>();
-                    e = e.field("Enchantments", item_enchantments.join(", "), false);
-                }
-
-                e
-            })
-        })
-        .await?;
     handle_autodelete(ctx, msg).await?;
 
     Ok(())
